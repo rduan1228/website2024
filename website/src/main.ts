43b0a2a9f246b59fd2f2aa69b2f24e88da4b3ed1
EgeKeskin <e.keskin@wustl.edu>
2024-06-08 15:31:11 -0500

    Remove deleted files

--- website/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import './assets/reset.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
--- website/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  // TODO: REMOVE WHEN SETTING CUSTOM DOMAIN
-  base: './',
-  plugins: [vue()],
-})
